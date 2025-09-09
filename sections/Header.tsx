9987c943d4af8b6c8b548a4958aa0b1a19cd59a9
Rafael Silva <rafaelbueno195@gmail.com>
2025-09-09 13:20:28 +0000

    Publish 1 from environment_v4 into origin/main

--- sections/Header.tsx
+++ sections/Header.tsx
@@ -67,40 +67,6 @@ export default function Haader({
 }: Nav) {
   return (
     <nav class="container mx-auto lg:px-0 px-4">
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-NGXBTVNKMV"></script>
-
-      <script dangerouslySetInnerHTML={{__html: `
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'G-NGXBTVNKMV', {
-          send_page_view: true
-        });
-      `}}></script>
-
-      <script dangerouslySetInnerHTML={{
-        __html: `
-        !function(f,b,e,v,n,t,s)
-        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-        n.queue=[];t=b.createElement(e);t.async=!0;
-        t.src=v;s=b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t,s)}(window, document,'script',
-        'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '866557691268690');
-        fbq('track', 'PageView');
-      `}}></script>
-
-      <script dangerouslySetInnerHTML={{__html: `
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'AW-11155911943');
-      `}}></script>
-
-
       <div class="flex gap-8 items-center justify-between py-4">
         <a href="/">
           <Image src={logo.src || ""} width={100} height={28} alt={logo.alt} />
